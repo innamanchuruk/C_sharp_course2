9b72555dd2d9cce9f43238bba9d9874be02a559a
inna <inna-portnoi@mail.ru>
2022-07-03 12:02:46 +0300

    task_h3

---
+++ 2_Les/HW_3/Program.cs
@@ -0,0 +1,14 @@
+﻿void ThirdNum(int num)
+{
+    Console.WriteLine($"{num} -> ");
+    if (num < 99)
+    {
+         Console.WriteLine("there is no third digit");
+         return;
+    }
+
+    while (num > 999) num = num / 10;
+     Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()));
