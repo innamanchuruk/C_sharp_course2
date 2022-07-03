7a34a1f8e6a6dea477aa5ccbe8c2d2abb4c31885
inna <inna-portnoi@mail.ru>
2022-07-03 11:22:33 +0300

    task2_2

---
+++ 2_Les/2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿void OneOfNum(int num_1)
+{
+    if (num_1 % 7 == 0 && num_1 % 23 == 0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
+
+OneOfNum(16);
+OneOfNum(161);
