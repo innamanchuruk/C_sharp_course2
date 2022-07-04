24f5d278b43f5e2344420554a19435743748d8c0
inna <inna-portnoi@mail.ru>
2022-07-04 16:40:58 +0300

    HW_3

---
+++ Lesson_3/HW_3/Program.cs
@@ -0,0 +1,19 @@
+﻿void Squares(int n)
+{
+    double[] nums = new double[n];
+    int index = 0;
+
+    Console.WriteLine($"{n} -> ");
+
+    while (index < n)
+    {
+        nums[index] = Math.Pow((index + 1), 3);
+        Console.Write($"{nums[index]}, ");
+        index += 1;
+    }
+    Console.WriteLine();
+}
+
+Squares(6);
+Squares(2);
+Squares(11);
