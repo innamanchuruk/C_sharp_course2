cdc8771318d135d3bfc8dd02e6343ba9312f840f
inna <inna-portnoi@mail.ru>
2022-07-08 21:01:34 +0300

    HW4_2

---
+++ 4_Lesson/HW4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNums(int num)
+{
+    int n_sum = 0;
+
+    while (num > 0)
+    {
+        n_sum += num % 10;
+        num /= 10;
+    }
+    return n_sum;
+}
+
+Console.WriteLine(SumNums(452));
+Console.WriteLine(SumNums(82));
+Console.WriteLine(SumNums(9012));
