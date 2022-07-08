016b7db4de4e0e7d241418cc873a4bb761fddad0
inna <inna-portnoi@mail.ru>
2022-07-08 20:51:28 +0300

    HW4_1

---
+++ 4_Lesson/HW4_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int PowNum(int a, int b)
+{
+    int n_pow = a;
+
+    for (int i = 2; i < b; i++)
+    {
+        n_pow *= a;
+    }
+    return n_pow;
+}
+
+Console.WriteLine(PowNum(3, 5));
+Console.WriteLine(PowNum(2, 4));
+
