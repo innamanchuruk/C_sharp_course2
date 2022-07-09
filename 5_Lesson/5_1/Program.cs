afbad5db72a7d8fd44ca359850cdf530c8009b2f
inna <inna-portnoi@mail.ru>
2022-07-09 10:17:41 +0300

    task5_1

---
+++ 5_Lesson/5_1/Program.cs
@@ -0,0 +1,37 @@
+﻿void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+    return arr;
+}
+
+void Change(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] arr_1 = MassNums(12);
+Print(arr_1);
+Change(arr_1);
+Print(arr_1);
+
+
+
