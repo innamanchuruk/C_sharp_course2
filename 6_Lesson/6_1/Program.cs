6b11b9d1ab5f0f7abd06e8284c2d1327dfd3b210
inna <inna-portnoi@mail.ru>
2022-07-17 14:00:25 +0300

    task6_1

---
+++ 6_Lesson/6_1/Program.cs
@@ -0,0 +1,8 @@
+﻿void Triangle (int numA,int numB, int numC)
+{
+    if(numA + numB > numC && numA + numC > numB && numB + numC > numA)
+    Console.WriteLine("yes!");
+    else Console.WriteLine("No!");
+}
+
+Triangle(1, 2, 3);
