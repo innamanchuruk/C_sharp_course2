126c5af4b98ccd86cc1249c9864dc7032fe587d5
inna <inna-portnoi@mail.ru>
2022-07-17 16:06:26 +0300

    HW_6_2

---
+++ 6_Lesson/HW_6_2/Program.cs
@@ -0,0 +1,8 @@
+﻿void Intersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Intersection point: ({x}; {y})");
+}
+
+Intersection(2, 5, 4, 9);
