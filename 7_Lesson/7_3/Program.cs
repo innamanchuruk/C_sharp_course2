cfb5320be82b50560497b86a95e8f1c40c772a58
inna <inna-portnoi@mail.ru>
2022-07-18 13:18:55 +0300

    7_3

---
+++ 7_Lesson/7_3/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] CreateEmpty2DArray(int rowsNumber, int columnsNamber)
+{
+    return new int[rowsNumber, columnsNamber];
+}
+
+void Fill2DArray(int[,] array2D, int minBorderForRandom, int maxBorderForRandom)
+{
+    int rowsNumber = array2D.GetLength(0);
+    int columnsNamber = array2D.GetLength(1);
+
+    for (int row_index = 0; row_index < rowsNumber; row_index++)
+    {
+        for (int column_index = 0; column_index < columnsNamber; column_index++)
+        {
+            array2D[row_index, column_index] = new Random().Next(minBorderForRandom, maxBorderForRandom);
+        }
+    }
+}
+
+void Print2DArray(int[,] array2D)
+{
+    int rowsNumber = array2D.GetLength(0);
+    int columnsNumber = array2D.GetLength(1);
+
+    for (int row_index = 0; row_index < rowsNumber; row_index++)
+    {
+        for (int column_index = 0; column_index < columnsNumber; column_index++)
+        {
+            Console.Write($"{array2D[row_index, column_index],4:D} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Modify2DArray(int[,] array2D)
+{
+    int rowsNumber = array2D.GetLength(0);
+    int columnsNamber = array2D.GetLength(1);
+
+    for (int row_index = 0; row_index < rowsNumber; row_index++)
+    {
+        for (int column_index = 0; column_index < columnsNamber; column_index++)
+        {
+            if ((row_index + 1) % 2 == 0 && (column_index + 1) % 2 == 0)
+            {
+                array2D[row_index, column_index] *= array2D[row_index, column_index];
+            }
+        }
+    }
+}
+
+    int[,] array2D = CreateEmpty2DArray(3, 3);
+    Fill2DArray(array2D, 1, 10);
+    Print2DArray(array2D);
+    Modify2DArray(array2D);
+    Console.WriteLine();
+    Print2DArray(array2D);
