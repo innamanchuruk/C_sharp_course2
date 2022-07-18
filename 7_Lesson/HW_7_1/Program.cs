601cbbda50ceb6326a25c818a18eb9eef65fc13d
inna <inna-portnoi@mail.ru>
2022-07-18 16:29:19 +0300

    HW_7_1

---
+++ 7_Lesson/HW_7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.WriteLine($" {arr[i, j],6} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * -10, 2);
+    return arr;
+}
+
+double[,] arr_1 = MassNums(3, 4, -10, 8);
+Print(arr_1);
+double[,] arr_2 = MassNums(5, 6, -6, 7);
+Print(arr_2);
