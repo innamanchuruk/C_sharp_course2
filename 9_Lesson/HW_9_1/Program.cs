090429e2a244ebedefaa94d1ced252e7bdaed80f
inna <inna-portnoi@mail.ru>
2022-07-21 19:20:47 +0300

    HW_9_1

---
+++ 9_Lesson/HW_9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5 -> " 2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Sequence(int M, int N)
+{
+    if (N < M) return;
+    Sequence(M, N-1);
+    Console.Write($"{N}, ");
+}
+Sequence(M, N);
+
+
